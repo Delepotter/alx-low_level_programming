ac041fd99c1508c374118a1b0e5df31582f22075
delepotter <felixokonmah4@gmail.com>
2023-03-05 11:34:42 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
+This line includes the main.h header file, which contains the prototype for the swap_int function.
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
-
-int _strlen(char *s)
+This is a documentation comment that describes what the swap_int function does. The comment includes the name of the function, a brief description, and the parameter descriptions.
+void swap_int(int *a, int *b)
+This line defines the swap_int function. It takes two integer pointers as its parameters, a and b, which are denoted by the int *a and int *b syntax. The function returns nothing, which is indicated by the void keyword.
 {
-    int longi = 0;
+    int m;
 
-    while (*s != '\0')
-    {
-        longi++;
-        s++;
-    }
-
-    return (longi);
+    m = *a;
+    *a = *b;
+    *b = m;
 }
